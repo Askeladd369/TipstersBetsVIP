9358b953e62e0fddb1f060aac99df9248a8b9198
Askeladd369 <146481623+Askeladd369@users.noreply.github.com>
2024-09-04 23:51:19 -0600

    Add files via upload

--- TipstersBet/config.py
+++ TipstersBet/config.py
@@ -4,10 +4,10 @@ api_hash = "c110fb4d3ba8473643b8e33e1c81be1d"
 bot_token = "7020407274:AAE6sGek2yA-AbgJjsNmyvkTyCDoErAaxFM"
 
 # IDs de los canales privados
-channel_nacionales = -1002023819899  # Reemplaza con el ID del canal para Tipsters Nacionales
-channel_americanos = -1001987602366  # Reemplaza con el ID del canal para Tipsters Americanos
-channel_europeos = -1002120180361  # Reemplaza con el ID del canal para Tipsters Europeos
-channel_alta_efectividad = -1002131916769  # Reemplaza con el ID del canal para Grupo Alta Efectividad
+channel_nacionales = -1002002230427  # Reemplaza con el ID del canal para Tipsters Nacionales
+channel_americanos = -1001956367839  # Reemplaza con el ID del canal para Tipsters Americanos
+channel_europeos = -1002139886828  # Reemplaza con el ID del canal para Tipsters Europeos
+channel_alta_efectividad = -1002054332150  # Reemplaza con el ID del canal para Grupo Alta Efectividad
 
 admin_id = 1142604997  # Tu ID de Telegram
 admin_id2 = 1209577470  # Lista de IDs de administradores secundarios
