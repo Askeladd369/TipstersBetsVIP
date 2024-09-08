3a4984376bb40c751b98ceb1ea2e9f3968913d91
Askeladd369 <146481623+Askeladd369@users.noreply.github.com>
2024-09-08 15:42:41 -0600

    Add files via upload

--- TipstersBet/config.py
+++ TipstersBet/config.py
@@ -11,16 +11,13 @@ channel_alta_efectividad = -1002054332150  # Reemplaza con el ID del canal para 
 
 admin_id = 1142604997  # Tu ID de Telegram
 admin_id2 = 1209577470  # Lista de IDs de administradores secundarios
-bot_id= 7165468466
+
 excel_path ="C:\\Users\\Administrator\\TipstersBetsVIP\\TipstersBet\\excel tipstersbets.xlsx"
+watermark_path="C:\\Users\\Administrator\\TipstersBetsVIP\\TipstersBet\\watermark.png"
 
-"C:\\Users\\saidd\\OneDrive\\Escritorio\\Bot de Telegram pruebas\\TipstersBet\\watermark.png"
 
+CANAL_PRIVADO_ID = -1002431937420
+
+"C:\\Users\\saidd\OneDrive\\Escritorio\\Bot de Telegram pruebas\\TipstersBet\\excel tipstersbets.xlsx"
 "C:\\Users\\Administrator\\SendTipsters\\Bot Reventas\\excel.xlsx"
-
-"C:\\Users\\saidd\\OneDrive\\Escritorio\\Bot de Telegram pruebas\\Bot Reventas\\excel.xlsx"
-
-"C:\\Users\\Administrator\\SendTipsters\\Bot Reventas\\el de los picks.png"
-
-"C:\\Users\\Administrator\\SendTipsters\\Bot Reventas\\master.png"
 
